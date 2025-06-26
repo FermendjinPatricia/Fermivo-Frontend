0fea63617d62f8402a84a7f295eb4a6b133a2262
FermendjinPatricia <91600978+FermendjinPatricia@users.noreply.github.com>
2025-06-26 11:43:16 +0300

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,8 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+self.__WB_MANIFEST;
+
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
