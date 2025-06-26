7a8834f4c6b36ad989d2863d2236d7ff300e9406
FermendjinPatricia <91600978+FermendjinPatricia@users.noreply.github.com>
2025-06-26 11:41:26 +0300

    update

---
+++ public/s-W.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'fermivo-v2'; // Nume al cache version
+self.__WB_MANIFEST; // Manifestul service worker-ului, generat de Workbox
+const urlsToCache = [ // Lista de URL-uri de cache
+  '/',
+  '/index.html', // Pagina principală
+  '/manifest.json', // Manifestul aplicației web
+  '/icons/icon-192x192.png', 
+  '/icons/icon-512x512.png',
+  '/assets/login.jpg', // Imaginea pentru autentificare
+  '/assets/register.jpg', // Imaginea pentru înregistrare
+  '/offline.html', // Pagina de rezervă pentru offline
+];
+self.addEventListener('install', (event) => { // Evenimentul de instalare a service worker-ului
+    self.skipWaiting();
+  event.waitUntil( // Așteaptă până când cache-ul este creat
+    caches.open(CACHE_NAME).then((cache) => { // Deschide cache-ul specificat
+      return cache.addAll(urlsToCache); // Adaugă URL-urile specificate în cache
+    })
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+  const url = event.request.url;
+  if (url.startsWith("chrome-extension://")) return;
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+      return fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          // Clonează răspunsul și cache-uiește-l
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => {
+        if (event.request.headers.get('accept')?.includes('text/html')) {
+          return caches.match('/offline.html');
+        }
+      });
+    })
+  );
+});
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    ).then(() => self.clients.claim()) // ✅ Ia controlul imediat
+  );
+});
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,47 @@
-const CACHE_NAME = 'fermivo-v2'; // Nume al cache version
-self.__WB_MANIFEST; // Manifestul service worker-ului, generat de Workbox
-const urlsToCache = [ // Lista de URL-uri de cache
-  '/',
-  '/index.html', // Pagina principală
-  '/manifest.json', // Manifestul aplicației web
-  '/icons/icon-192x192.png', 
-  '/icons/icon-512x512.png',
-  '/assets/login.jpg', // Imaginea pentru autentificare
-  '/assets/register.jpg', // Imaginea pentru înregistrare
-  '/offline.html', // Pagina de rezervă pentru offline
-];
-self.addEventListener('install', (event) => { // Evenimentul de instalare a service worker-ului
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html ";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
-  event.waitUntil( // Așteaptă până când cache-ul este creat
-    caches.open(CACHE_NAME).then((cache) => { // Deschide cache-ul specificat
-      return cache.addAll(urlsToCache); // Adaugă URL-urile specificate în cache
-    })
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
 self.addEventListener('fetch', (event) => {
-  const url = event.request.url;
-  if (url.startsWith("chrome-extension://")) return;
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) return response;
-      return fetch(event.request).then((response) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          // Clonează răspunsul și cache-uiește-l
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      }).catch(() => {
-        if (event.request.headers.get('accept')?.includes('text/html')) {
-          return caches.match('/offline.html');
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
-      });
-    })
-  );
-});
 
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    ).then(() => self.clients.claim()) // ✅ Ia controlul imediat
-  );
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
